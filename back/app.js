06026654be81bcb2000b290e815a80edea0e012e
CarlosFTG <37017033+CarlosFTG@users.noreply.github.com>
2019-01-20 11:44:11 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -11,15 +11,6 @@ var app = express()
 
   app.use(corsMiddleware) 
   app.options('*', corsMiddleware) 
-//CONFIGURAR CABECERAS HTTP
-/* app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://wheatherapp.com');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-}); */
-
 
 
 // load routes
