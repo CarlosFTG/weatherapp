80c8a7cc5164feda3ed5848e97684d12432cca1b
CarlosFTG <37017033+CarlosFTG@users.noreply.github.com>
2019-01-20 11:44:39 +0100

    Update index.js

--- back/index.js
+++ back/index.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var app = require('./app.js');
 var port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://admin:AranjueZ86!!@ds259410.mlab.com:59410/weatherapp', (err, res) => {
+mongoose.connect('mongodb://admin:xxxxx@xxxxxx.mlab.com:xxxxx/weatherapp', (err, res) => {
     if(err){
         throw err;
     }else{
